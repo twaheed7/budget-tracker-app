f3836f4b6dde38929456e434dbde8eacbc827f53
twaheed7 <tayyubwaheed@outlook.com>
2023-03-30 16:22:09 +0300

    setup

---
+++ src/components/Budget.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Budget = () => {
+  return (
+    <div className="alert alert-secondary">
+      <span>Budget: 1000qr</span>
+    </div>
+  );
+};
+export default Budget;
