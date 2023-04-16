2e5f5d2297ee7a2c6e9eb17ac359ace7f1f1226c
twaheed7 <tayyubwaheed@outlook.com>
2023-04-16 16:11:34 +0300

    wrap app around AppProvider

--- src/App.js
+++ src/App.js
@@ -10,32 +10,34 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   return (
-    <div className="container">
-      <h1 className="mt-3">Budget Tracker</h1>
-      <div className="row mt-3">
-        <div className="col-sm">
-          <Budget />
+    <AppProvider>
+      <div className="container">
+        <h1 className="mt-3">Budget Tracker</h1>
+        <div className="row mt-3">
+          <div className="col-sm">
+            <Budget />
+          </div>
+          <div className="col-sm">
+            <Remaining />
+          </div>
+          <div className="col-sm">
+            <ExpenseTotal />
+          </div>
         </div>
-        <div className="col-sm">
-          <Remaining />
+        <h3 className="mt-3">Expenses</h3>
+        <div className="row mt-3">
+          <div className="col-sm">
+            <ExpenseList />
+          </div>
         </div>
-        <div className="col-sm">
-          <ExpenseTotal />
+        <h3 className="mt-3">Add Expense</h3>
+        <div className="row mt-3">
+          <div className="col-sm">
+            <AddExpenseForm />
+          </div>
         </div>
       </div>
-      <h3 className="mt-3">Expenses</h3>
-      <div className="row mt-3">
-        <div className="col-sm">
-          <ExpenseList />
-        </div>
-      </div>
-      <h3 className="mt-3">Add Expense</h3>
-      <div className="row mt-3">
-        <div className="col-sm">
-          <AddExpenseForm />
-        </div>
-      </div>
-    </div>
+    </AppProvider>
   );
 };
 
